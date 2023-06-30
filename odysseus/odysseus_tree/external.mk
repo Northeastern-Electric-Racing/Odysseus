4eb05c93dc5b59ba0e8806b9a6fd7b243b15d15a
Nick <nwdepatie@gmail.com>
2023-06-30 17:07:54 -0400

    Testing external module building

--- odysseus/odysseus_tree/external.mk
+++ odysseus/odysseus_tree/external.mk
@@ -1,1 +1,1 @@
-include $(sort $(wildcard $(BR2_EXTERNAL_BAR_42_PATH)/package/*/*.mk))
+include $(sort $(wildcard $(BR2_EXTERNAL_ODY_TREE_PATH)/package/*/*.mk))
---
+++ odysseus/odysseus_tree/package/nrc-cli-app/nrc-cli.mk
@@ -0,0 +1,14 @@
+NRC7292_MODULE_VERSION = 1.0
+NRC7292_SITE = $(BR2_EXTERNAL_ODY_TREE_PATH)/src/nrc7292_sw_pkg/package/src/cli_app
+NRC7292_SITE_METHOD = local
+NRC7292_DEPENDENCIES = ncurses
+
+define ${PKGNAME}_BUILD_CMDS
+   $(MAKE) $(TARGET_CONFIGURE_OPTS) all -C $(@D)
+endef
+define ${PKGNAME}_INSTALL_TARGET_CMDS
+   $(INSTALL) -D -m 0755 $(@D)/cli_app $(TARGET_DIR)/usr/bin
+endef
+${PKGNAME}_LICENSE = Proprietary
+
+$(eval $(generic-package))

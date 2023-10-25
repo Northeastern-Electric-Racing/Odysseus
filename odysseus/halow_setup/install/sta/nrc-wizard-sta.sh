e1875b2bed86cf39b0bdfc1b9e82baa2123c89c8
Jack Rubacha <rubacha.jack03@gmail.com>
2023-10-24 21:57:08 -0400

    fully split ap/sta scripts into two, make all scrits executable

--- odysseus/halow_setup/install/sta/nrc-wizard-sta.sh
+++ odysseus/halow_setup/install/sta/nrc-wizard-sta.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # the parameters to pass into start.py for sta mode
-STA_START_PARAMETERS="0 0 US"
-# the parameters to pass into stop.py for ap mode
-AP_START_PARAMETERS="1 0 US"
-# sleep time to ensure booted
+START_PARAMETERS="0 0 US"
+# check every x seconds if the nrc system has booted
 SLEEP_TIME=1
 
 
@@ -13,13 +11,13 @@ if [ "$EUID" -eq 0 ]; then
   exit 1
 fi
 
-if [ "$1" == "sta-start" ];
+if [ "$1" == "start" ];
 then
-    python -u $HOME/nrc_pkg/script/start.py $STA_START_PARAMETERS
-elif [ "$1" == "sta-start-systemd" ];
+    python -u $HOME/nrc_pkg/script/start.py $START_PARAMETERS
+elif [ "$1" == "start-systemd" ];
 then
     echo "Begin start.py ----------------"
-    python -u $HOME/nrc_pkg/script/start.py $STA_START_PARAMETERS &
+    python -u $HOME/nrc_pkg/script/start.py $START_PARAMETERS &
     # capture start.py pid
     start_pid="$!"
     while true;
@@ -27,7 +25,7 @@ then
       sleep $SLEEP_TIME
       # get the latest string in startup-logs beginining in [DIGIT]
       # example : [7] Connect and DHCP
-      latest=$(/usr/local/sbin/nrc-wizard.sh sta-startup-logs | grep -o -P "\[\d\].*" | tail -n 1)
+      latest=$(/usr/local/sbin/nrc-wizard-sta.sh startup-logs | grep -o -P "\[\d\].*" | tail -n 1)
     
       # update system status with latest message
       systemd-notify --status="Currently on $latest"
@@ -39,7 +37,7 @@ then
           break
       elif ! [ -d "/proc/$start_pid" ]; # if start.py has finished running
       then # refresh status message and exit the script, telling systemd to shutdown and cleanup
-          latest=$(/usr/local/sbin/nrc-wizard.sh sta-startup-logs | grep -o -P "\[\d\].*" | tail -n 1)
+          latest=$(/usr/local/sbin/nrc-wizard-sta.sh startup-logs | grep -o -P "\[\d\].*" | tail -n 1)
     
           # update system status with latest message
           systemd-notify --status="Failed on $latest"
@@ -49,14 +47,11 @@ then
     done
     
     sleep infinity
-elif [ "$1" == "ap-start" ] || [ "$1" == "ap-start-systemd" ];
-then
-    python -u $HOME/nrc_pkg/script/start.py $AP_START_PARAMETERS
 elif [ "$1" == "stop" ];
 then
     echo "Begin stop.py -----------------"
     python -u $HOME/nrc_pkg/script/stop.py
-elif [ "$1" == "sta-startup-logs" ];
+elif [ "$1" == "startup-logs" ];
 then 
   # this displays the most recent invocation ID of nrc-autostart and filters the logs by that invocation, otherwise journalctl would display all logs since nrc-autostart was first started.  It also passes in any other parameters given to journalctl.
   
@@ -65,13 +60,6 @@ then
   shift;
   
   journalctl -u nrc-autostart-sta.service  "$@" -e _SYSTEMD_INVOCATION_ID="$(systemctl show -p InvocationID --value nrc-autostart-sta.service)"
-elif [ "$1" == "ap-startup-logs" ];
-then 
-  # see above ^^^, this is just switching the service names
-  echo "$@"
-  shift;
-  
-  journalctl -u nrc-autostart-ap.service  "$@" -e _SYSTEMD_INVOCATION_ID="$(systemctl show -p InvocationID --value nrc-autostart-ap.service)"
 else
-  echo "Run with <sta|ap>-start|stop|<sta|ap>-startup-logs to configure start.py/stop.py accordingly, or get logs"
+  echo "FOR STA MODE: Run with start|stop|startup-logs to configure start.py/stop.py accordingly, or get logs"
 fi
--- odysseus/halow_setup/install_sta_startup.sh
+++ odysseus/halow_setup/install_sta_startup.sh
@@ -5,27 +5,23 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-echo "Installing systemd file and associated commands"
-cd ./install
+echo "For STA Mode: Installing systemd file and associated commands"
+cd ./install/sta
 
-chmod +x ./nrc-led-ap.sh
 chmod +x ./nrc-led-sta.sh
-chmod + ./nrc-wizard.sh
+chmod + ./nrc-wizard-sta.sh
 
 # /usr/local/sbin is standard for user installed scripts, it is in path
-cp ./nrc-wizard.sh /usr/local/sbin
+cp ./nrc-wizard-sta.sh /usr/local/sbin
 
 cp ./nrc-led-sta.sh /usr/local/sbin
-cp ./nrc-led-ap.sh /usr/local/sbin
 
 cp ./nrc-autostart-sta.service /etc/systemd/system/
-cp ./nrc-autostart-ap.service /etc/systemd/system
 
 cp ./nrc-led-sta.service /etc/systemd/system/
-cp ./nrc-led-ap.service /etc/systemd/system/
 
 # refresh systemd to pick up new unit files
 systemctl daemon-reload
 
 
-echo "Done! Run \"systemctl enable nrc-autostart-<sta|ap>\" to have it run upon boot"
+echo "Done! Run \"systemctl enable nrc-autostart-sta\" to have it run upon boot"

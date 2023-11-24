e89877da27309ff5e1132be6dceaa7344fc43b28
Nicholas DePatie <80368116+nwdepatie@users.noreply.github.com>
2023-11-23 22:02:35 -0500

    Mqtt (#32)
    Adding in sample publishers, subscribers, and configuration for Mosquitto MQTT broker for basic messaging
    
    ---------
    
    Co-authored-by: Jack R <rubacha.jack03@gmail.com>
    Co-authored-by: Evan <evan.lombardo04@gmail.com>

---
+++ siren/conf_update_pi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# this script copies the configuration files into their FS appropriate positions for use with debian mosquitto installs + systemd.
+
+if [ "$EUID" -ne 0 ]; then
+  echo "You are not root, exiting."
+  exit 1
+fi
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+
+echo "Copying confs"
+cp "$SCRIPT_DIR/mosquitto.conf" "/etc/mosquitto/conf.d/siren.conf"
+cp "$SCRIPT_DIR/acl.conf" "/etc/mosquitto/acl.conf"
+
+# change the acl_file location to be correct, specifically regex matches all acl_file at beginning of line, then replaces whole line with acl_file /etc/mosquitto/acl.conf
+sed -i '/^acl_file*/s/.*/acl_file\ \/etc\/mosquitto\/acl.conf/' "/etc/mosquitto/conf.d/siren.conf"

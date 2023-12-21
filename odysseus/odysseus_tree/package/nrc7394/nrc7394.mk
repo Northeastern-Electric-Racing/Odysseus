be6bb77a4b90d36f61b4c0b43ea2905f889ffa17
Jack Rubacha <rubacha.jack03@gmail.com>
2023-12-20 19:05:45 -0500

    add nrc7394 package and submodule

---
+++ odysseus/odysseus_tree/package/nrc7394/nrc7394.mk
@@ -0,0 +1,42 @@
+NRC7394_PROVIDER_PROVIDES = nrc-module
+# match upstream sw_pkg version
+NRC7394_VERSION = 1.2
+NRC7394_SITE = $(BR2_EXTERNAL_ODY_TREE_PATH)/sources/nrc7394_sw_pkg/package/src/nrc
+NRC7394_SITE_METHOD = local
+NRC7394_LICENSE = LGPLv2.1/GPLv2
+# require dtc for overlay compilation
+NRC7394_DEPENDENCIES += host-dtc
+# set the makefile KDIR to buildroot kernel, as otherwise it will use host headers
+NRC7394_MODULE_MAKE_OPTS = KDIR=$(LINUX_DIR)
+
+# for some reason host-objtool is being blocked as dependency conflict, so this is a hacky way to make it available again
+define NRC7394_MODULE_LINUX_OBJTOOL_FIX
+   $(MAKE) -C $(LINUX_DIR)/tools/objtool
+endef
+
+# use default source dto file if not specified
+BR2_PACKAGE_NRC_CUSTOM_DTOVERLAY_FILE ?= $(BR2_EXTERNAL_ODY_TREE_PATH)/sources/nrc7394_sw_pkg/dts/newracom_for_5.16_or_later.dts
+define NRC7394_BUILD_DTO
+	# compile dto
+	$(HOST_DIR)/usr/bin/dtc -@ -I dts -O dtb -o $(@D)/newracom.dtbo $(BR2_PACKAGE_NRC_CUSTOM_DTOVERLAY_FILE)
+	# put dto in the rpi-firmware directory such that it will install it in the imaging stage 
+	$(INSTALL) -D -m 0644 $(@D)/newracom.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/newracom.dtbo
+endef
+
+# set custom bd file to default (evk) if unset, also set firmware file (unchainging)
+BR2_PACKAGE_NRC7394_CUSTOM_BD_FILE ?=  $(BR2_EXTERNAL_ODY_TREE_PATH)/sources/nrc7394_sw_pkg/package/evk/binary/nrc7394_bd.dat
+NRC7394_FIRMWARE_FILE = $(BR2_EXTERNAL_ODY_TREE_PATH)/sources/nrc7394_sw_pkg/package/evk/binary/nrc7394_cspi.bin
+
+define NRC7394_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0644 $(BR2_PACKAGE_NRC7394_CUSTOM_BD_FILE) $(TARGET_DIR)/lib/firmware/nrc7394_bd.dat
+	$(INSTALL) -D -m 0644 $(NRC7394_FIRMWARE_FILE) $(TARGET_DIR)/lib/firmware/nrc7394_cspi.bin
+endef
+
+NRC7394_PRE_BUILD_HOOKS += NRC7394_MODULE_LINUX_OBJTOOL_FIX
+
+NRC7394_POST_BUILD_HOOKS += NRC7394_BUILD_DTO
+
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
+

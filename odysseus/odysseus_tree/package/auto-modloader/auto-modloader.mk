81129c9c57865707812658b4fe3155c4b65d93bc
Jack Rubacha <rubacha.jack03@gmail.com>
2023-12-20 19:05:31 -0500

    refactor + split leds, userspace packages for ap/sta

--- odysseus/odysseus_tree/package/auto-modloader/auto-modloader.mk
+++ odysseus/odysseus_tree/package/auto-modloader/auto-modloader.mk
@@ -1,8 +1,9 @@
 AUTO_MODLOADER_VERSION = 1.0
-AUTO_MODLOADER_SOURCE =
+AUTO_MODLOADER_SITE = $(BR2_EXTERNAL_ODY_TREE_PATH)/package/auto-modloader
+AUTO_MODLOADER_SITE_METHOD = local
 
 define AUTO_MODLOADER_INSTALL_INIT_SYSV
-    $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_ODY_TREE_PATH)/package/auto-modloader/S04modules $(TARGET_DIR)/etc/init.d/S04modules
+    $(INSTALL) -D -m 0755 $(@D)/S04modules $(TARGET_DIR)/etc/init.d/S04modules
 endef
 
 $(eval $(generic-package))

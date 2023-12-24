bad39dd7280633e2c261d86ceda818debd1f8ef1
Jack Rubacha <rubacha.jack03@gmail.com>
2023-12-24 17:49:32 -0500

    improve rpi-dtloader, switch newracom dtoverlays to that
    rationale for removing dtoverlay from nrc-module: the overlay file is user responsibility in other distros, so it should not be bundled with the kernel module install in buildroot.

--- odysseus/odysseus_tree/package/nrc7292/nrc7292.mk
+++ odysseus/odysseus_tree/package/nrc7292/nrc7292.mk
@@ -14,15 +14,6 @@ define NRC7292_MODULE_LINUX_OBJTOOL_FIX
    $(MAKE) -C $(LINUX_DIR)/tools/objtool
 endef
 
-# use default source dto file if not specified
-BR2_PACKAGE_NRC_CUSTOM_DTOVERLAY_FILE ?= $(BR2_EXTERNAL_ODY_TREE_PATH)/sources/nrc7292_sw_pkg/dts/newracom_for_5.16_or_later.dts
-define NRC7292_BUILD_DTO
-	# compile dto
-	$(HOST_DIR)/usr/bin/dtc -@ -I dts -O dtb -o $(@D)/newracom.dtbo $(BR2_PACKAGE_NRC_CUSTOM_DTOVERLAY_FILE)
-	# put dto in the rpi-firmware directory such that it will install it in the imaging stage 
-	$(INSTALL) -D -m 0644 $(@D)/newracom.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/newracom.dtbo
-endef
-
 # set custom bd file to default (evk) if unset, also set firmware file (unchainging)
 BR2_PACKAGE_NRC7292_CUSTOM_BD_FILE ?=  $(BR2_EXTERNAL_ODY_TREE_PATH)/sources/nrc7292_sw_pkg/package/evk/binary/nrc7292_bd.dat
 NRC7292_FIRMWARE_FILE = $(BR2_EXTERNAL_ODY_TREE_PATH)/sources/nrc7292_sw_pkg/package/evk/binary/nrc7292_cspi.bin

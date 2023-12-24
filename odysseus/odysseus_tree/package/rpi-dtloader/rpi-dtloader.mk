75d56e374d66a5925a6ed710b0d0efaffd10f603
Jack Rubacha <rubacha.jack03@gmail.com>
2023-12-24 17:49:32 -0500

    improve rpi-dtloader, switch newracom dtoverlays to that
    rationale for removing dtoverlay from nrc-module: the overlay file is user responsibility in other distros, so it should not be bundled with the kernel module install in buildroot.

--- odysseus/odysseus_tree/package/rpi-dtloader/rpi-dtloader.mk
+++ odysseus/odysseus_tree/package/rpi-dtloader/rpi-dtloader.mk
@@ -3,9 +3,17 @@ RPI_DTLOADER_SITE = $(BR2_EXTERNAL_ODY_TREE_PATH)/package/rpi-dtloader
 RPI_DTLOADER_SITE_METHOD = local
 RPI_DTOVERLAY_DEPENDENCIES += host-dtc
 
-define RPI_DTLOADER_INSTALL_TARGET_CMDS
+define RPI_DTLOADER_BUILD_CMDS
+	# iterate through dts files, write to $(@D)/file_name.dtbo
+	# basename strips extension, notdir strips everything but file name
 	$(foreach dts,$(call qstrip,$(BR2_PACKAGE_RPI_DTLOADER_FILES)), \
-		$(HOST_DIR)/usr/bin/dtc -@ -I dts -O dtb -o $(@D)/$(notdir $(basename $(dts))).dtbo $(dts) && \
+		$(HOST_DIR)/usr/bin/dtc -@ -I dts -O dtb -o $(@D)/$(notdir $(basename $(dts))).dtbo $(dts)
+	)
+endef
+
+define RPI_DTLOADER_INSTALL_TARGET_CMDS
+	# fetch the built dtbos and install them read only to rpi-firmware image prep dir
+	$(foreach dts,$(call qstrip,$(BR2_PACKAGE_RPI_DTLOADER_FILES)), \
 		$(INSTALL) -D -m 0644 $(@D)/$(notdir $(basename $(dts))).dtbo $(BINARIES_DIR)/rpi-firmware/overlays/
 	)
 endef

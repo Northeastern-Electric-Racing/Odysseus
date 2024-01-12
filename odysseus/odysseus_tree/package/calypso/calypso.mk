2637d6279b7ba4ff4e1d7d9a8e7c65b67a3fcef8
Jack Rubacha <rubacha.jack03@gmail.com>
2024-01-12 15:38:58 -0500

    builds cleanly

--- odysseus/odysseus_tree/package/calypso/calypso.mk
+++ odysseus/odysseus_tree/package/calypso/calypso.mk
@@ -1,9 +1,9 @@
-CALYPSO_VERSION = 0.1.0
+CALYPSO_VERSION = 20ae5443e5e6626fd4bda12131d9e496b9faf34b
 CALYPSO_SITE_METHOD = git
 CALYPSO_SITE = https://github.com/Northeastern-Electric-Racing/Calypso
 
 define CALYPSO_INSTALL_INIT_SYSV
-    $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_ODY_TREE_PATH)/package/calypso/S74calypso $(TARGET_DIR)/etc/init.d/S74calypso
+    $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_ODY_TREE_PATH)/package/calypso/S71calypso $(TARGET_DIR)/etc/init.d/S71calypso
 endef
 
 $(eval $(cargo-package))

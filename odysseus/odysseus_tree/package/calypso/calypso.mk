87a61d14790b482ffe8ec76e6eb7cd3ecd583e21
Jack Rubacha <rubacha.jack03@gmail.com>
2024-01-13 18:06:47 -0500

    calypso working
    see nanomq issue! probably not from calypso

--- odysseus/odysseus_tree/package/calypso/calypso.mk
+++ odysseus/odysseus_tree/package/calypso/calypso.mk
@@ -1,9 +1,10 @@
-CALYPSO_VERSION = 20ae5443e5e6626fd4bda12131d9e496b9faf34b
+CALYPSO_VERSION = 8b7ef49ad94703907ec3029f04fa664c7ba5f734
 CALYPSO_SITE_METHOD = git
 CALYPSO_SITE = https://github.com/Northeastern-Electric-Racing/Calypso
+CALYPSO_DEPENDENCIES += openssl
 
 define CALYPSO_INSTALL_INIT_SYSV
-    $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_ODY_TREE_PATH)/package/calypso/S71calypso $(TARGET_DIR)/etc/init.d/S71calypso
+    $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_ODY_TREE_PATH)/package/calypso/S76calypso $(TARGET_DIR)/etc/init.d/S76calypso
 endef
 
 $(eval $(cargo-package))

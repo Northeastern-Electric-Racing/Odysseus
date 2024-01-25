582f296373fbba89777ca745e7756b354a910b3a
Jack Rubacha <rubacha.jack03@gmail.com>
2024-01-25 18:50:05 -0500

    patch dhcpcd rather than change version

--- odysseus/odysseus_tree/pre-make-common.sh
+++ odysseus/odysseus_tree/pre-make-common.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-
-echo "change dhcpcd version to 10.0.5 from 10.0.4"
-cd $SCRIPT_DIR/../buildroot/package/dhcpcd/ || exit
-# remove integrity file for dhcpcd 10.0.4
-rm ./dhcpcd.hash
-# replace old version with new one
-sed -i 's/DHCPCD_VERSION = 10.0.4/DHCPCD_VERSION = 10.0.5/g' dhcpcd.mk
+# SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+# 
+# echo "change dhcpcd version to 10.0.5 from 10.0.4"
+# cd $SCRIPT_DIR/../buildroot/package/dhcpcd/ || exit
+# # remove integrity file for dhcpcd 10.0.4
+# rm ./dhcpcd.hash
+# # replace old version with new one
+# sed -i 's/DHCPCD_VERSION = 10.0.4/DHCPCD_VERSION = 10.0.5/g' dhcpcd.mk
 

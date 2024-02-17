281ce26921a0267800194b17d9d9ed15514d9e60
Jack Rubacha <rubacha.jack03@gmail.com>
2024-02-16 19:28:01 -0500

    rules setup and important fix

--- odysseus/odysseus_tree/package/usbip/usbip.mk
+++ odysseus/odysseus_tree/package/usbip/usbip.mk
@@ -2,11 +2,15 @@ USBIP_AUTORECONF = YES
 USBIP_CONF_OPTS = --without-tcp-wrappers
 USBIP_DEPENDENCIES = linux udev
 
-USBIP_SRC_DIR = $(wildcard \
-  $(LINUX_DIR)/tools/usb/usbip \
-  $(LINUX_DIR)/drivers/staging/usbip/userspace)
+USBIP_SRC_DIR = $(LINUX_DIR)/tools/usb/usbip
 
+# not best practice, but dont remove this line or a rsync infinite recursion would be triggered
+USBIP_EXTRACT_DEPENDENCIES = linux
+
+
+# overwrite autotools extraction infra
 define USBIP_EXTRACT_CMDS
+	# rip usbip from linux source to avoid double clone, put it where autotools expects
 	rsync -au --chmod=u=rwX,go=rX $(RSYNC_VCS_EXCLUSIONS) $(USBIP_SRC_DIR)/ $(@D)
 endef
 

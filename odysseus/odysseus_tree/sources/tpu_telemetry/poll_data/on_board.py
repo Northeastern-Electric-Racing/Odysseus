d4fac53b2161f161e9e0ea6fb912c3eafce22ae9
Ha Nguyen <nganha.nguyen3011@gmail.com>
2024-02-19 00:04:23 -0500

    clean code

--- odysseus/odysseus_tree/sources/tpu_telemetry/poll_data/on_board.py
+++ odysseus/odysseus_tree/sources/tpu_telemetry/poll_data/on_board.py
@@ -4,20 +4,19 @@
 import psutil, sys
 
 # fetch_data() -> List[(str, [str], str)]
-# finish after testing
+
 def fetch_data():
     return[fetch_cpu_temperature(), fetch_cpu_usage(), fetch_broker_cpu_usage(), fetch_available_memory()]
     
 
 
-#CPU Temp
+# CPU Temp
 def fetch_cpu_temperature():
     temps = psutil.sensors_temperatures(fahrenheit=False)
     
     if not temps:
         sys.exit("can't read any temperature")
     for name, entries in temps.items():
-        print(name)
         for entry in entries:
             line = "    %-20s %s °C (high = %s °C, critical = %s °C)" % (
                 entry.label or name,
@@ -25,12 +24,10 @@ def fetch_cpu_temperature():
                 entry.high,
                 entry.critical,
             )
-            print(line)
-        print() #remove after testing
     
     return[("TPU/OnBoard/CpuTemp", [entry.current], "celsius")]
 
-#CPU Usage 
+# CPU Usage 
 def fetch_cpu_usage():
     try:
         cpu_usage = psutil.cpu_percent()
@@ -47,7 +44,6 @@ def fetch_broker_cpu_usage():
             pid = int(file.read())
             process = psutil.Process(pid)
             broker_cpu_usage = process.cpu_percent()
-            print(broker_cpu_usage)
         return[("TPU/OnBoard/BrokerCpuUsage", [broker_cpu_usage], "%")]
     except Exception as e:
         print(f"Error fetching nanomq broker CPU usage: {e}")
@@ -58,7 +54,6 @@ def fetch_available_memory():
     try:
         mem_info = psutil.virtual_memory()
         mem_available = mem_info.available / (1024 * 1024)  
-        print(mem_available) #remove after testing
         return[("TPU/OnBoard/MemAvailable", mem_available, "MB")]
     except Exception as e:
         print(f"Error fetching available memory: {e}")

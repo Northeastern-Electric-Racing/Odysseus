18a7db399d61f17e462dd1228c5a9f8872602312
Jack Rubacha <rubacha.jack03@gmail.com>
2024-02-20 18:20:11 -0500

    add protobuf package, nero builds

--- odysseus/odysseus_tree/package/nero2/nero2.mk
+++ odysseus/odysseus_tree/package/nero2/nero2.mk
@@ -2,7 +2,7 @@ NERO2_VERSION = 1.0
 NERO2_SITE = $(BR2_EXTERNAL_ODY_TREE_PATH)/sources/Nero-2.0/NERODevelopment
 NERO2_SITE_METHOD = local
 NERO2_CMAKE_BACKEND = ninja
-NERO2_DEPENDENCIES += nerqt6base nerqt6declarative nerqt6mqtt
+NERO2_DEPENDENCIES += nerqt6base nerqt6declarative nerqt6mqtt nerqt6grpc
 
 define NERO2_INSTALL_INIT_SYSV
     $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_ODY_TREE_PATH)/package/nero2/S99nero2 $(TARGET_DIR)/etc/init.d/S99nero2
---
+++ odysseus/odysseus_tree/package/nerqt6/nerqt6grpc/nerqt6grpc.mk
@@ -0,0 +1,50 @@
+################################################################################
+#
+# nerqt6grpc (NER: new package!)
+#
+################################################################################
+
+NERQT6GRPC_VERSION = $(NERQT6_VERSION)
+NERQT6GRPC_SITE = $(NERQT6_SITE)
+NERQT6GRPC_SOURCE = qtgrpc-$(NERQT6_SOURCE_TARBALL_PREFIX)-$(NERQT6GRPC_VERSION).tar.xz
+NERQT6GRPC_INSTALL_STAGING = YES
+NERQT6GRPC_SUPPORTS_IN_SOURCE_BUILD = NO
+
+NERQT6GRPC_CMAKE_BACKEND = ninja
+
+NERQT6GRPC_LICENSE = \
+	GPL-3.0-only WITH Qt-GPL-exception-1.0, \
+	GPL-2.0-only or LGPL-3.0-only, \
+	GFDL-1.3-no-invariants-only, \
+	Apache-2.0 or MIT, \
+	BSD-3-Clause, \
+	BSD-2-Clause, \
+	Apache-2.0, \
+	GPL-3.0-or-later WITH Bison-Exception-2.2
+
+NERQT6GRPC_LICENSE_FILES = \
+	LICENSES/BSD-3-Clause.txt \
+	LICENSES/GFDL-1.3-no-invariants-only.txt \
+	LICENSES/GPL-2.0-only.txt \
+	LICENSES/GPL-3.0-only.txt \
+	LICENSES/LGPL-3.0-only.txt \
+	LICENSES/Qt-GPL-exception-1.0.txt \
+	src/3rdparty/SPIRV-Cross/KHRONOS-LICENSE.txt \
+	src/3rdparty/SPIRV-Cross/LICENSE \
+	src/3rdparty/glslang/LICENSE.txt
+
+NERQT6GRPC_CONF_OPTS = \
+	-DBUILD_WITH_PCH=OFF \
+	-DQT_BUILD_EXAMPLES=OFF \
+	-DQT_BUILD_TESTS=OFF
+
+NERQT6GRPC_DEPENDENCIES = \
+	host-pkgconf \
+	nerqt6base \
+	host-nerqt6grpc \
+	protobuf
+	
+HOST_NERQT6GRPC_DEPENDENCIES = host-nerqt6base host-protobuf
+
+$(eval $(cmake-package))
+$(eval $(host-cmake-package))

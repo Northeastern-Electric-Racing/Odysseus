6dcc3f3ffc36b5ae020e5251aa00c052d8ee9e66
starmut <57048370+starmut@users.noreply.github.com>
2024-03-04 16:11:59 -0500

    telemetry decorator, staggering (#124)
    * telemetry decorator, staggering
    no idea if this works
    
    * sleep forever after starting intervals
    
    * fix improts, type hints
    
    * fixing dep issues
    
    * make it stop
    
    * works?
    
    * make unused warnings dissapear
    it needs to be imported for it to be populated in the dict.
    
    * make all internal imports relative, fix host, add freq, add missing halow measure
    
    ---------
    
    Co-authored-by: Jack Rubacha <rubacha.jack03@gmail.com>

--- odysseus/odysseus_tree/sources/tpu_telemetry/telemetry/publish.py
+++ odysseus/odysseus_tree/sources/tpu_telemetry/telemetry/publish.py
@@ -1,73 +1,77 @@
 import asyncio
 import signal
-from telemetry import server_data_pb2
-from poll_data import example, can, on_board, halow
-#from gen_data import server_data_pb2
-import time
-import asyncio
+from . import (
+    server_data_pb2,
+    routines,
+    set_interval,
+    poll_data as _,  # your editor lies, this is an important import.
+)
 from gmqtt import Client as MQTTClient
 
 # initialize connection
-
 client = MQTTClient("tpu-publisher")
-
 STOP = asyncio.Event()
 
+
 def on_connect(client, flags, rc, properties):
-    print('Connected')
+    print("Connected")
+
 
 def on_disconnect(client, packet, exc=None):
-    print('Disconnected')
-    
+    print("Disconnected")
+
+
 def ask_exit(*args):
     STOP.set()
+
 
 def publish_data(topic, message_data):
     # send the data
     client.publish(topic, message_data)
+
+
+async def interval(fn, freq):
+    async for result in set_interval(fn, freq, STOP):
+        for packet in result:
+            print(packet)
+
+            data = server_data_pb2.ServerData()
+            topic, values, data.unit = packet
+
+            for val in values:
+                data.value.append(val)
+
+            message_data = data.SerializeToString()
+            publish_data(topic, message_data)
+
 
 async def run(host):
     await client.connect(host, 1883)
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
 
-    while True:
-        items = can.fetch_data() + halow.fetch_data() + on_board.fetch_data()
+    stagger = 1 / len(routines)
 
-        for file_data in items:
-            print(file_data)
-            topic = file_data[0]
+    for fn in routines:
+        freq = routines[fn]
 
-            data = server_data_pb2.ServerData()
-
-            data.unit = file_data[2]
-
-            values = file_data[1]
-            for val in values:
-                data.value.append(val)
-            
-            message_data = data.SerializeToString()
-            
-            publish_data(topic, message_data)
-            
-        if STOP.is_set():
-            await client.disconnect()
-            return
-
-        await asyncio.sleep(1)
+        # should not be awaited, this just gets run parallely along with other intervals.
+        asyncio.create_task(interval(fn, freq))
+        await asyncio.sleep(stagger)
 
     await STOP.wait()
 
 
 def main():
     loop = asyncio.new_event_loop()
-    
-    host = '127.0.0.1'
+
+    host = "localhost"
 
     loop.add_signal_handler(signal.SIGINT, ask_exit)
     loop.add_signal_handler(signal.SIGTERM, ask_exit)
 
     loop.run_until_complete(run(host))
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     main()

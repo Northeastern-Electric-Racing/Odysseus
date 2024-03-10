c00f1fa7e93abd54712ee3edc5c44521ed0eeeee
Jack Rubacha <rubacha.jack03@gmail.com>
2024-03-10 19:52:23 -0400

    cleanup all docker scripting (#127)
    * cleanup all docker scripting
    
    * add iroh, fix nero?
    
    * rename docker_out_of_tree

--- odysseus/docker_out_of_tree.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# for each defconfig make output subdirectory
-cd /home/odysseus/build/buildroot
-make O=/home/odysseus/outputs/tpu BR2_EXTERNAL=/home/odysseus/build/odysseus_tree raspberrypi4_64_tpu_defconfig
-make O=/home/odysseus/outputs/ap BR2_EXTERNAL=/home/odysseus/build/odysseus_tree raspberrypi3_64_ap_defconfig
-make O=/home/odysseus/outputs/iroh BR2_EXTERNAL=/home/odysseus/build/odysseus_tree raspberrypi3_64_iroh_defconfig

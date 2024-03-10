6a3fece96db44ea99666df6f66a70406e4163cb2
Jack Rubacha <rubacha.jack03@gmail.com>
2024-03-10 19:40:45 -0400

    add iroh, fix nero?

--- odysseus/docker_scripts/docker_out_of_tree.sh
+++ odysseus/docker_scripts/docker_out_of_tree.sh
@@ -5,6 +5,7 @@ alias make-current="/home/odysseus/scripts/make-current.sh"
 # for each defconfig make output subdirectory
 make -C /home/odysseus/build/buildroot O=/home/odysseus/outputs/tpu BR2_EXTERNAL=/home/odysseus/build/odysseus_tree raspberrypi4_64_tpu_defconfig
 make -C /home/odysseus/build/buildroot O=/home/odysseus/outputs/ap BR2_EXTERNAL=/home/odysseus/build/odysseus_tree raspberrypi3_64_ap_defconfig
+make -C /home/odysseus/build/buildroot O=/home/odysseus/outputs/iroh BR2_EXTERNAL=/home/odysseus/build/odysseus_tree raspberrypi3_64_iroh_defconfig
 
 
 echo \

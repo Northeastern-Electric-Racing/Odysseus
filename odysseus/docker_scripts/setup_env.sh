be6736a2a8895bf3b6e958bc876d0d6e4bde1119
Jack Rubacha <rubacha.jack03@gmail.com>
2024-03-12 11:25:48 -0400

    ascii goes hard

--- odysseus/docker_scripts/setup_env.sh
+++ odysseus/docker_scripts/setup_env.sh
@@ -8,10 +8,12 @@ make -C /home/odysseus/build/buildroot O=/home/odysseus/outputs/ap BR2_EXTERNAL=
 make -C /home/odysseus/build/buildroot O=/home/odysseus/outputs/iroh BR2_EXTERNAL=/home/odysseus/build/odysseus_tree raspberrypi3_64_iroh_defconfig
 
 
-echo \
-    "
-    ██████  ██████  ██    ██ ███████ ███████ ███████ ██    ██ ███████
-   ██    ██ ██   ██  ██  ██  ██      ██      ██      ██    ██ ██     
-   ██    ██ ██   ██   ████   ███████ ███████ █████   ██    ██ ███████
-   ██    ██ ██   ██    ██         ██      ██ ██      ██    ██      ██
-    ██████  ██████     ██    ███████ ███████ ███████  ██████  ███████"
+cat << "EOF"
+ .d88b.  d8888b. db    db .d8888. .d8888. d88888b db    db .d8888. 
+.8P  Y8. 88  `8D `8b  d8' 88'  YP 88'  YP 88'     88    88 88'  YP 
+88    88 88   88  `8bd8'  `8bo.   `8bo.   88ooooo 88    88 `8bo.   
+88    88 88   88    88      `Y8b.   `Y8b. 88      88    88   `Y8b. 
+`8b  d8' 88  .8D    88    db   8D db   8D 88.     88b  d88 db   8D 
+ `Y88P'  Y8888D'    YP    `8888Y' `8888Y' Y88888P ~Y8888P' `8888Y'
+
+EOF

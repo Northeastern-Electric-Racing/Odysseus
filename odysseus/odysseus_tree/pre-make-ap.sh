798ce2c4dbf3356298ef0304f5fde9ca969b9fa0
Jack Rubacha <rubacha.jack03@gmail.com>
2024-03-23 20:36:30 -0400

    misc nrc params fixes, nero debug update

--- odysseus/odysseus_tree/pre-make-ap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-python3 "$BR2_EXTERNAL_ODY_TREE_PATH"/utils/build_nrc_params.py 1 "$BR2_EXTERNAL_ODY_TREE_PATH"/overlays/rootfs_overlay_ap/etc/nrc_opts_ap.ini "$BR2_EXTERNAL_ODY_TREE_PATH"/overlays/rootfs_overlay_ap/etc/modprobe.d/nrc.conf

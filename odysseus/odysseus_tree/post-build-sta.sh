403a74a68756d532a6053407ae958e10d2b0992b
Jack Rubacha <rubacha.jack03@gmail.com>
2024-03-28 18:15:54 -0400

    add wifi password infra

--- odysseus/odysseus_tree/post-build-sta.sh
+++ odysseus/odysseus_tree/post-build-sta.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+# add nrc modprobe based on ini config
 python3 "$BR2_EXTERNAL_ODY_TREE_PATH"/overlays/rootfs_overlay_nrc_common/usr/bin/build_nrc_params.py 0 "$BR2_EXTERNAL_ODY_TREE_PATH"/overlays/rootfs_overlay_tpu/etc/nrc_opts_sta.ini "$TARGET_DIR"/etc/modprobe.d/nrc.conf

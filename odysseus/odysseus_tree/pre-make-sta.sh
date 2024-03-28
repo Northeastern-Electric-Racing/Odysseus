49e0d2d7cf009450f77327283539707ee9df8efc
Jack Rubacha <rubacha.jack03@gmail.com>
2024-03-28 17:05:27 -0400

    Introduce macOS (partial) and Windows support (#125)
    * add debug defconfig, remove symlink
    
    * Update README.md
    
    * eol gitattr
    
    * add note about nanomq
    
    * misc nrc params fixes, nero debug update

--- odysseus/odysseus_tree/pre-make-sta.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-python3 "$BR2_EXTERNAL_ODY_TREE_PATH"/utils/build_nrc_params.py 0 "$BR2_EXTERNAL_ODY_TREE_PATH"/overlays/rootfs_overlay_tpu/etc/nrc_opts_sta.ini "$BR2_EXTERNAL_ODY_TREE_PATH"/overlays/rootfs_overlay_tpu/etc/modprobe.d/nrc.conf

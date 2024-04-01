1ebd44a5c586a458e3d9dfa1a8b28fbd1af94eb7
Jack Rubacha <rubacha.jack03@gmail.com>
2024-04-01 19:13:22 -0400

    initial update, br0 differentiation not yet ready

--- odysseus/odysseus_tree/package/alfa-led/alfa-led-ap.sh
+++ odysseus/odysseus_tree/package/alfa-led/alfa-led-ap.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
+# AP MODE led script for ALFA AHPI7292 w/ router relayed station active-backup bonding indication
 ######################
-# amount of time (in seconds) to wait in between loops
-SLEEP_TIME=1.5
-#ap interface, wlan0 for default newracom configuration
-INTERFACE_NAME=wlan0
-# the ethernet interface to check if an ip addr has been attained, for yellow light
-YELLOW_LED_INTERFACE_NAME=eth0
+# amount of time to wait in between loops
+SLEEP_TIME=1
+# backup interface in bond
+BACKUP_INTERFACE_NAME=wlan0
+# primary interface in bond, an ip as its router relayed
+PRIMARY_INTERFACE_IP=192.168.100.12
 # path to NRC cli app
 CLI_APP="/usr/bin/cli_app"
+# mac addr prefix of station halow wifi
+HALOW_MAC_ADDR=00:c0:ca
 ######################
-
-
 
 # bit flipper for blinking function
 oscillate=0
@@ -21,7 +22,7 @@ $CLI_APP gpio direction 3 1
 $CLI_APP gpio direction 2 1
 
 # pin 3 = red led (RX)
-# pin 2 = yellow red (TX)
+# pin 2 = yellow led (TX)
 
 # a cleanup script run upon any exit (like ctrl^c), to turn the lights off and not leave them on and unresponsive.  Should run before module is unloaded.
 trap cleanup INT HUP TERM
@@ -31,26 +32,123 @@ cleanup() {
     exit
 }
 
-echo "Starting ACCESS POINT Mode LEDs"
+echo "Starting AP Mode LEDs"
 while true;
 do
     sleep "$SLEEP_TIME"s
-    # see list of connected clients (if non empty)
-    # check gateway for ip being given
-    clients_present=$(hostapd_cli list_sta -i $INTERFACE_NAME)
-    if [ -n "$clients_present" ];
-    then
-        $CLI_APP gpio write 3 1 >> /dev/null
-    else
-        oscillate="$((1-oscillate))"
-        $CLI_APP gpio write 3 "$oscillate" >> /dev/null
+
+    # check if ping is successful
+    is_present=0
+    ping -c 1 "$PRIMARY_INTERFACE_IP" > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+         is_present=1
     fi
 
-    ip -json address show "$YELLOW_LED_INTERFACE_NAME" | jq -e '.[0].addr_info' >> /dev/null
-    if [ "$?" -ne 0 ];
+    # if both interfaces are down, warn by short yellow
+    if [ "$is_present" -eq 0 ]
+    then
+        oscillate="$((1-oscillate))"
+        $CLI_APP gpio write 2 "$oscillate" >> /dev/null
+        $CLI_APP gpio write 3 0 >> /dev/null
+        continue
+    fi
+
+    is_halow_connected=0
+    # blink red if backup is linked, solid red if currently active
+    if grep -q "$HALOW_MAC_ADDR" <(hostapd_cli list_sta)
+    then
+        $CLI_APP gpio write 3 1 >> /dev/null
+#    elif [ "$is_present" -eq 1]
+#    then
+#       oscillate="$((1-oscillate))"
+#        $CLI_APP gpio write 3 "$oscillate" >> /dev/null
+    else
+        $CLI_APP gpio write 3 0 >> /dev/null
+    fi
+
+    # solid yellow if primary is linked, which implies it must be active
+    if [ "$is_present" -eq 1 ]
     then
         $CLI_APP gpio write 2 1 >> /dev/null
     else
         $CLI_APP gpio write 2 0 >> /dev/null
     fi
+
+done
+#!/bin/sh
+
+# AP MODE led script for ALFA AHPI7292 w/ router relayed station active-backup bonding indication
+######################
+# amount of time to wait in between loops
+SLEEP_TIME=1
+# backup interface in bond
+BACKUP_INTERFACE_NAME=wlan0
+# primary interface in bond, an ip as its router relayed
+PRIMARY_INTERFACE_IP=192.168.100.12
+# path to NRC cli app
+CLI_APP="/usr/bin/cli_app"
+# mac addr prefix of station halow wifi
+HALOW_MAC_ADDR=00:c0:ca
+######################
+
+# bit flipper for blinking function
+oscillate=0
+
+# set the correct pin directions, must run before a write
+$CLI_APP gpio direction 3 1
+$CLI_APP gpio direction 2 1
+
+# pin 3 = red led (RX)
+# pin 2 = yellow led (TX)
+
+# a cleanup script run upon any exit (like ctrl^c), to turn the lights off and not leave them on and unresponsive.  Should run before module is unloaded.
+trap cleanup INT HUP TERM
+cleanup() {
+    $CLI_APP gpio write 3 0
+    $CLI_APP gpio write 2 0
+    exit
+}
+
+echo "Starting AP Mode LEDs"
+while true;
+do
+    sleep "$SLEEP_TIME"s
+
+    # check if ping is successful
+    is_present=0
+    ping -c 1 "$PRIMARY_INTERFACE_IP" > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+         is_present=1
+    fi
+
+    # if both interfaces are down, warn by short yellow
+    if [ "$is_present" -eq 0 ]
+    then
+        oscillate="$((1-oscillate))"
+        $CLI_APP gpio write 2 "$oscillate" >> /dev/null
+        $CLI_APP gpio write 3 0 >> /dev/null
+        continue
+    fi
+
+    is_halow_connected=0
+    # blink red if backup is linked, solid red if currently active
+    if grep -q "$HALOW_MAC_ADDR" <(hostapd_cli list_sta)
+    then
+        $CLI_APP gpio write 3 1 >> /dev/null
+#    elif [ "$is_present" -eq 1]
+#    then
+#       oscillate="$((1-oscillate))"
+#        $CLI_APP gpio write 3 "$oscillate" >> /dev/null
+    else
+        $CLI_APP gpio write 3 0 >> /dev/null
+    fi
+
+    # solid yellow if primary is linked, which implies it must be active
+    if [ "$is_present" -eq 1 ]
+    then
+        $CLI_APP gpio write 2 1 >> /dev/null
+    else
+        $CLI_APP gpio write 2 0 >> /dev/null
+    fi
+
 done

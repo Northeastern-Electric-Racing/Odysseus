ee1e98c1f504b2d9ba3d60f6b0a234b99273f0d6
Jack Rubacha <rubacha.jack03@gmail.com>
2024-08-28 18:04:09 -0400

    fix nrc

--- odysseus_tree/package/nrc7292/nrc7292.mk
+++ odysseus_tree/package/nrc7292/nrc7292.mk
@@ -10,8 +10,8 @@ NRC7292_MODULE_SUBDIRS = "package/src/nrc"
 NRC7292_MODULE_MAKE_OPTS = KDIR=$(LINUX_DIR)
 
 # set custom bd file to default (evk) if unset, also set firmware file (unchainging)
-BR2_PACKAGE_NRC7292_CUSTOM_BD_FILE ?=  $(BR2_EXTERNAL_ODY_TREE_PATH)/sources/nrc7292_sw_pkg/package/evk/binary/nrc7292_bd.dat
-NRC7292_FIRMWARE_FILE = $(BR2_EXTERNAL_ODY_TREE_PATH)/sources/nrc7292_sw_pkg/package/evk/binary/nrc7292_cspi.bin
+BR2_PACKAGE_NRC7292_CUSTOM_BD_FILE ?=  $(@D)/package/evk/binary/nrc7292_bd.dat
+NRC7292_FIRMWARE_FILE = $(@D)/package/evk/binary/nrc7292_cspi.bin
 
 define NRC7292_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(BR2_PACKAGE_NRC7292_CUSTOM_BD_FILE) $(TARGET_DIR)/lib/firmware/nrc7292_bd.dat

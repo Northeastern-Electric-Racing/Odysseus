897e98c1b2f95573e8e1adc2e302028bf3c8765b
Jack Rubacha <rubacha.jack03@gmail.com>
2024-10-02 17:32:27 -0400

    Make AP a argos node (#168)
    * add chrony
    
    * further fixes, switch ap
    
    * misc fixes, bump calypso for reconnect, add time back to iroh
    
    * fix ip fowarding

--- odysseus_tree/post-build-base-connect.sh
+++ odysseus_tree/post-build-base-connect.sh
@@ -2,7 +2,7 @@
 
 # hashes the password so it cannot be read in a dsitributed image (does not secure wifi!)
 # this regex extras the part after psk=
-hashed_block=$(wpa_passphrase "H68K" "$ODY_BASE_WIFI_PASSWORD" | sed  -n -e 's/^.*[[:space:]]psk=//p')
+hashed_block=$(wpa_passphrase "Hermes" "$ODY_BASE_WIFI_PASSWORD" | sed  -n -e 's/^.*[[:space:]]psk=//p')
 
 # this replaces the psk in the target directory with the hashed block found above
 sed -i "s/\(psk=\)\(.*\)/\1$hashed_block/" "$TARGET_DIR"/etc/wpa_supplicant_base.conf

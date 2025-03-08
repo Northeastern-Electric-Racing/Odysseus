69469881ea98d6a2834e4049b8d415f942fc0f7e
Jack Rubacha <rubacha.jack03@gmail.com>
2025-03-07 22:15:23 -0500

    siren av

--- odysseus_tree/package/odysseus-daemon/odysseus-daemon.mk
+++ odysseus_tree/package/odysseus-daemon/odysseus-daemon.mk
@@ -1,4 +1,4 @@
-ODYSSEUS_DAEMON_VERSION = 5e071a3b36f8f1437ab5efeee29e8a8258e4ff63
+ODYSSEUS_DAEMON_VERSION = da32a5b07f15bfbeb7ff2b06f51b1f78d9a19490
 ODYSSEUS_DAEMON_SITE_METHOD = git
 ODYSSEUS_DAEMON_SITE = https://github.com/Northeastern-Electric-Racing/Odysseus-Daemon
 

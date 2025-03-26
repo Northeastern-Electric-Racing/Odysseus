5dcb10d26026d621c15c8d40079032a6636ae46d
Jack Rubacha <rubacha.jack03@gmail.com>
2025-03-26 14:31:56 -0400

    fixup name

--- odysseus_tree/post-build-base-connect.sh
+++ odysseus_tree/post-build-base-connect.sh
@@ -7,7 +7,7 @@ hashed_block=$(wpa_passphrase "Hermes" "$ODY_BASE_WIFI_PASSWORD" | sed  -n -e 's
 # this replaces the psk in the target directory with the hashed block found above, only the first occurance (meaning Hermes must come before HermesField)
 sed -i "0,/psk=/{s/psk=.*/psk=$hased_block/}" "$TARGET_DIR"/etc/wpa_supplicant_base.conf
 
-hashed_block=$(wpa_passphrase "HermesField" "$ODY_BASE_WIFI_FIELD_PASSWORD" | sed  -n -e 's/^.*[[:space:]]psk=//p')
+hashed_block=$(wpa_passphrase "HermesField" "$ODY_BASE_FIELD_WIFI_PASSWORD" | sed  -n -e 's/^.*[[:space:]]psk=//p')
 # do the same thing in reverse
 tac "$TARGET_DIR"/etc/wpa_supplicant_base.conf | sed "0,/psk=/{s/psk=.*/psk=$hased_block/}" | tac > "$TARGET_DIR"/etc/wpa_supplicant_base.conf
 

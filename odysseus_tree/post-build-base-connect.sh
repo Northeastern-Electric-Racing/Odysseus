7fa56a6aab0bed81cf0cc0195ceec2f9e395bdba
Jack Rubacha <rubacha.jack03@gmail.com>
2025-03-26 13:21:53 -0400

    support hermes field

--- odysseus_tree/post-build-base-connect.sh
+++ odysseus_tree/post-build-base-connect.sh
@@ -4,5 +4,10 @@
 # this regex extras the part after psk=
 hashed_block=$(wpa_passphrase "Hermes" "$ODY_BASE_WIFI_PASSWORD" | sed  -n -e 's/^.*[[:space:]]psk=//p')
 
-# this replaces the psk in the target directory with the hashed block found above
-sed -i "s/\(psk=\)\(.*\)/\1$hashed_block/" "$TARGET_DIR"/etc/wpa_supplicant_base.conf
+# this replaces the psk in the target directory with the hashed block found above, only the first occurance (meaning Hermes must come before HermesField)
+sed -i "0,/psk=/{s/psk=.*/psk=$hased_block/}" "$TARGET_DIR"/etc/wpa_supplicant_base.conf
+
+hashed_block=$(wpa_passphrase "HermesField" "$ODY_BASE_WIFI_FIELD_PASSWORD" | sed  -n -e 's/^.*[[:space:]]psk=//p')
+# do the same thing in reverse
+tac "$TARGET_DIR"/etc/wpa_supplicant_base.conf | sed "0,/psk=/{s/psk=.*/psk=$hased_block/}" | tac > "$TARGET_DIR"/etc/wpa_supplicant_base.conf
+

0ef5d3df425eb8ddfabc0cc07090a9744dc3cd71
Jack Rubacha <rubacha.jack03@gmail.com>
2025-04-02 01:17:10 -0400

    use tmpfile redirect for fix

--- odysseus_tree/post-build-base-connect.sh
+++ odysseus_tree/post-build-base-connect.sh
@@ -5,9 +5,11 @@
 hashed_block=$(wpa_passphrase "Hermes" "$ODY_BASE_WIFI_PASSWORD" | sed  -n -e 's/^.*[[:space:]]psk=//p')
 
 # this replaces the psk in the target directory with the hashed block found above, only the first occurance (meaning Hermes must come before HermesField)
-sed -i "0,/psk=/{s/psk=.*/psk=$hased_block/}" "$TARGET_DIR"/etc/wpa_supplicant_base.conf
+sed -i "0,/psk=/{s/psk=.*/psk=$hashed_block/}" "$TARGET_DIR"/etc/wpa_supplicant_base.conf
 
-hashed_block=$(wpa_passphrase "HermesField" "$ODY_BASE_WIFI_PASSWORD" | sed  -n -e 's/^.*[[:space:]]psk=//p')
+hashed_block_2=$(wpa_passphrase "HermesField" "$ODY_BASE_WIFI_PASSWORD" | sed  -n -e 's/^.*[[:space:]]psk=//p')
 # do the same thing in reverse
-tac "$TARGET_DIR"/etc/wpa_supplicant_base.conf | sed "0,/psk=/{s/psk=.*/psk=$hased_block/}" | tac > "$TARGET_DIR"/etc/wpa_supplicant_base.conf
+tmpfile=$(mktemp)
+tac "$TARGET_DIR"/etc/wpa_supplicant_base.conf | sed "0,/psk=/{s/psk=.*/psk=$hashed_block_2/}" > $tmpfile
+tac $tmpfile > "$TARGET_DIR"/etc/wpa_supplicant_base.conf
 

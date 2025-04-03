0fb7190ac48d0171a11299bbf4f0fbdafc1796e0
Jack Rubacha <rubacha.jack03@gmail.com>
2025-04-03 19:20:22 -0400

    Update odysseus-daemon.mk

--- odysseus_tree/package/odysseus-daemon/odysseus-daemon.mk
+++ odysseus_tree/package/odysseus-daemon/odysseus-daemon.mk
@@ -1,6 +1,7 @@
 ODYSSEUS_DAEMON_VERSION = da32a5b07f15bfbeb7ff2b06f51b1f78d9a19490
 ODYSSEUS_DAEMON_SITE_METHOD = git
 ODYSSEUS_DAEMON_SITE = https://github.com/Northeastern-Electric-Racing/Odysseus-Daemon
+ODYSSEUS_DAEMON_DEPENDENCIES += openssl
 
 # all dependencies and support scripts are in the TPU overlay as this package is TPU specifc
 

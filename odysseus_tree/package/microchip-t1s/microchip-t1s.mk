6ebfc3ebe5936c9ed4496e2f833f4a1ca190e3b5
Jack Rubacha <rubacha.jack03@gmail.com>
2025-08-10 11:41:21 -0400

    microchip builds

--- odysseus_tree/package/microchip-t1s/microchip-t1s.mk
+++ odysseus_tree/package/microchip-t1s/microchip-t1s.mk
@@ -1,13 +1,16 @@
-MICROCHIP_T1S_PROVIDER_PROVIDES = nrc-module
-# match upstream sw_pkg version
-MICROCHIP_T1S_VERSION = 1ac663c2f62a2c88ced004d961885019ab2e304b
-MICROCHIP_T1S_SITE = https://github.com/Northeastern-Electric-Racing/nrc7292_sw_pkg
-MICROCHIP_T1S_SITE_METHOD = git
+MICROCHIP_T1S_VERSION = 3.0.0
+MICROCHIP_T1S_SOURCE_BASE = EVB-LAN8670-USB_Linux_Driver_3v0
+MICROCHIP_T1S_SOURCE = $(MICROCHIP_T1S_SOURCE_BASE).zip
+MICROCHIP_T1S_SITE = https://ww1.microchip.com/downloads/aemDocuments/documents/AIS/ProductDocuments/CodeExamples
 MICROCHIP_T1S_LICENSE = LGPLv2.1/GPLv2
 
-MICROCHIP_T1S_MODULE_SUBDIRS = "package/src/nrc"
+MICROCHIP_T1S_MODULE_SUBDIRS = "$(MICROCHIP_T1S_SOURCE_BASE)/lan867x-linux-driver-3v0/linux-v6.14-support/"
 # set the makefile KDIR to buildroot kernel, as otherwise it will use host headers
 MICROCHIP_T1S_MODULE_MAKE_OPTS = KDIR=$(LINUX_DIR)
+
+define MICROCHIP_T1S_EXTRACT_CMDS
+    $(UNZIP) $(MICROCHIP_T1S_DL_DIR)/$(MICROCHIP_T1S_SOURCE) -d $(@D)
+endef
 
 
 $(eval $(kernel-module))

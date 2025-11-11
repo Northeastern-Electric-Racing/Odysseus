c087ff18830d71611b0e097553f3351740c07a0c
Jack Rubacha <rubacha.jack03@gmail.com>
2025-11-11 16:42:52 -0500

    initial in progress

--- docker_scripts/setup_env.sh
+++ docker_scripts/setup_env.sh
@@ -8,6 +8,8 @@ make -C /home/odysseus/build/buildroot O=/home/odysseus/outputs/tpu BR2_EXTERNAL
 make -C /home/odysseus/build/buildroot O=/home/odysseus/outputs/ap-pi4 BR2_EXTERNAL=/home/odysseus/build/odysseus_tree raspberrypi4_64_ap_defconfig
 make -C /home/odysseus/build/buildroot O=/home/odysseus/outputs/iroh BR2_EXTERNAL=/home/odysseus/build/odysseus_tree raspberrypi3_64_iroh_defconfig
 make -C /home/odysseus/build/buildroot O=/home/odysseus/outputs/ap-pi5 BR2_EXTERNAL=/home/odysseus/build/odysseus_tree raspberrypi5_ap_defconfig
+make -C /home/odysseus/build/buildroot O=/home/odysseus/outputs/tpu-cm5 BR2_EXTERNAL=/home/odysseus/build/odysseus_tree raspberrypicm5io_tpu_defconfig
+make -C /home/odysseus/build/buildroot O=/home/odysseus/outputs/wheel-cm5 BR2_EXTERNAL=/home/odysseus/build/odysseus_tree raspberrypicm5io_wheel_defconfig
 
 
 cat << "EOF"

29824aaa068904aa0c97ffc28c96b7b6477949be
Jack Rubacha <rubacha.jack03@gmail.com>
2026-02-13 13:10:46 -0500

    use calypso latest

--- odysseus_tree/package/calypso/calypso.mk
+++ odysseus_tree/package/calypso/calypso.mk
@@ -1,4 +1,4 @@
-CALYPSO_VERSION = a5cad32e2865d0c81537a94424bfdab5eb9d2907
+CALYPSO_VERSION = 5afdf0e6bacc629267b5aefd9b5ba3cd1deae179
 CALYPSO_SITE_METHOD = git
 CALYPSO_SITE = https://github.com/Northeastern-Electric-Racing/Calypso
 CALYPSO_GIT_SUBMODULES = YES

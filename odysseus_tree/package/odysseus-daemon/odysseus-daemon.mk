d36b36ef0111ec85fba4779b31fdd42703c13b92
Jack Rubacha <rubacha.jack03@gmail.com>
2026-02-26 00:39:49 -0500

    ody daemon on wheel for leds

--- odysseus_tree/package/odysseus-daemon/odysseus-daemon.mk
+++ odysseus_tree/package/odysseus-daemon/odysseus-daemon.mk
@@ -1,4 +1,4 @@
-ODYSSEUS_DAEMON_VERSION = e78afeabd60b241390acb80bd57e45dc812eca7a
+ODYSSEUS_DAEMON_VERSION = 55d03e596fac6b8601d93fb996072c1973a4d89f
 ODYSSEUS_DAEMON_SITE_METHOD = git
 ODYSSEUS_DAEMON_SITE = https://github.com/Northeastern-Electric-Racing/Odysseus-Daemon
 ODYSSEUS_DAEMON_DEPENDENCIES += openssl host-pkgconf udev

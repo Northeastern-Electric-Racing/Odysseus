dd2deed0bb70b49de92785d6f886c802902d920a
Jack Rubacha <rubacha.jack03@gmail.com>
2026-02-27 14:47:57 -0500

    lp5018 mapping verified

--- odysseus_tree/package/odysseus-daemon/odysseus-daemon.mk
+++ odysseus_tree/package/odysseus-daemon/odysseus-daemon.mk
@@ -1,4 +1,4 @@
-ODYSSEUS_DAEMON_VERSION = 55d03e596fac6b8601d93fb996072c1973a4d89f
+ODYSSEUS_DAEMON_VERSION = 15b3abcb2cccaa2c82a30258bb5a38e4e99cb331
 ODYSSEUS_DAEMON_SITE_METHOD = git
 ODYSSEUS_DAEMON_SITE = https://github.com/Northeastern-Electric-Racing/Odysseus-Daemon
 ODYSSEUS_DAEMON_DEPENDENCIES += openssl host-pkgconf udev

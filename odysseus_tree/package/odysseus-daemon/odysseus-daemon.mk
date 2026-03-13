051d78bb8949a6b4e61294dc1ed06b7c6b175ddc
Jack Rubacha <rubacha.jack03@gmail.com>
2026-03-13 17:15:35 -0400

    hotfix

--- odysseus_tree/package/odysseus-daemon/odysseus-daemon.mk
+++ odysseus_tree/package/odysseus-daemon/odysseus-daemon.mk
@@ -1,4 +1,4 @@
-ODYSSEUS_DAEMON_VERSION = 15b3abcb2cccaa2c82a30258bb5a38e4e99cb331
+ODYSSEUS_DAEMON_VERSION = 40adc7998612350beb174efc442e65da7f0f51a0
 ODYSSEUS_DAEMON_SITE_METHOD = git
 ODYSSEUS_DAEMON_SITE = https://github.com/Northeastern-Electric-Racing/Odysseus-Daemon
 ODYSSEUS_DAEMON_DEPENDENCIES += openssl host-pkgconf udev

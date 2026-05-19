17c8efd9756dad76802f7136d3c4932c810d5fa9
Jack Rubacha <rubacha.jack03@gmail.com>
2026-05-19 17:46:14 -0400

    oops and perms

--- odysseus_tree/post-build-hostapd-pass.sh
+++ odysseus_tree/post-build-hostapd-pass.sh
@@ -2,4 +2,4 @@
 
 # Add a password for wheel to hostapd
 
-sed -i "s|^wpa_passphrase=.*|wpa_passphrase=${ODY_WHEEL_AP_PASSWORD}|" /etc/hostapd/hostapd.conf
+sed -i "s|^wpa_passphrase=.*|wpa_passphrase=${ODY_WHEEL_AP_PASSWORD}|" "$TARGET_DIR"/etc/hostapd.conf

784a12feab635258ee54f7d848ba2b50e62037d5
Jack Rubacha <rubacha.jack03@gmail.com>
2026-05-23 21:44:34 -0400

    switch to local.mk

--- odysseus_tree/external.mk
+++ odysseus_tree/external.mk
@@ -1,6 +1,1 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_ODY_TREE_PATH)/package/*/*.mk))
-
-# Morse Hostap override
-WPA_SUPPLICANT_VERSION = 1.17.9
-WPA_SUPPLICANT_SITE_METHOD = git
-WPA_SUPPLICANT_SITE = https://github.com/MorseMicro/hostap

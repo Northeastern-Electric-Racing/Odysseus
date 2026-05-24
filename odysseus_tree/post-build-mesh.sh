083531ba5b31cf38a18f0309ec829ea6f8efc731
Jack Rubacha <rubacha.jack03@gmail.com>
2026-05-24 00:27:55 -0400

    fixes, sae password untested

--- odysseus_tree/post-build-mesh.sh
+++ odysseus_tree/post-build-mesh.sh
@@ -4,4 +4,7 @@
 sed -i "s|^\(ssid=.*_\).*|\1${ODY_MESH_ID}|" "$TARGET_DIR"/etc/hostapd.conf
 
 # change the static IP
-sed -i "s|^\(\s*static ip_address=10\.42\.0\.\)[0-9X]\+/24|\1${ODY_NODE_ID}/24|" "$TARGET_DIR"/etc/dhcpcd.conf
+sed -i "s|^\(\s*static ip_address=10\.42\.0\.\)[0-9X]\+/24|\1${ODY_MESH_ID}/24|" "$TARGET_DIR"/etc/dhcpcd.conf
+
+# add the sae password
+sed -i "s|^\(\s*sae_password=\)\".*\"|\1\"${ODY_MESH_SAE_PASSWORD}\"|" "$TARGET_DIR"/etc/wpa_supplicant_halow.conf

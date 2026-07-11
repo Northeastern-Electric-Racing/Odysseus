ae0554dfcca79b39a4c714ad0a7762c557601c95
Jack Rubacha <rubacha.jack03@gmail.com>
2026-07-11 11:22:19 -0400

    oops

--- odysseus_tree/package/zenohd/zenohd.mk
+++ odysseus_tree/package/zenohd/zenohd.mk
@@ -4,7 +4,7 @@ ZENOHD_SITE = https://github.com/eclipse-zenoh/zenoh
 ZENOHD_SUBDIR = ./zenohd
 
 define ZENOHD_INSTALL_INIT_SYSV
-    $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_ODY_TREE_PATH)/package/zenohd/S76zenohd $(TARGET_DIR)/etc/init.d/S76zenohd
+    $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_ODY_TREE_PATH)/package/zenohd/S50zenohd $(TARGET_DIR)/etc/init.d/S50zenohd
 endef
 
 $(eval $(cargo-package))

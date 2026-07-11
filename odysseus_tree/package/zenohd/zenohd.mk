0ed6a453db6797e59113ac381c62597e84267c10
Jack Rubacha <rubacha.jack03@gmail.com>
2026-07-11 11:20:02 -0400

    zenoh fix

--- odysseus_tree/package/zenohd/zenohd.mk
+++ odysseus_tree/package/zenohd/zenohd.mk
@@ -1,6 +1,7 @@
 ZENOHD_VERSION = 1.9.0
 ZENOHD_SITE_METHOD = git
 ZENOHD_SITE = https://github.com/eclipse-zenoh/zenoh
+ZENOHD_SUBDIR = ./zenohd
 
 define ZENOHD_INSTALL_INIT_SYSV
     $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_ODY_TREE_PATH)/package/zenohd/S76zenohd $(TARGET_DIR)/etc/init.d/S76zenohd

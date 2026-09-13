50ab3c71de6865f5564a457e3d24e3e55535839a
Jack Rubacha <rubacha.jack03@gmail.com>
2026-09-13 17:29:26 -0400

    switch to subdir

--- odysseus_tree/package/probe-rs/probe-rs.mk
+++ odysseus_tree/package/probe-rs/probe-rs.mk
@@ -3,8 +3,8 @@ PROBE_RS_SITE_METHOD = git
 PROBE_RS_SITE = https://github.com/probe-rs/probe-rs
 PROBE_RS_GIT_SUBMODULES = YES
 PROBE_RS_DEPENDENCIES = host-rustc libusb udev
-PROBE_RS_CARGO_BUILD_OPTS = -p probe-rs-tools --features remote
-PROBE_RS_CARGO_INSTALL_OPTS = --path ./probe-rs-tools
+PROBE_RS_CARGO_BUILD_OPTS = --features remote
+PROBE_RS_SUBDIR = probe-rs-tools
 
 
 define PROBE_RS_INSTALL_INIT_SYSV

6b67bd9799b5aaeb2f53ae99d31d22aa5f3b7283
Jack Rubacha <rubacha.jack03@gmail.com>
2026-09-13 17:27:55 -0400

    try override

--- odysseus_tree/package/probe-rs/probe-rs.mk
+++ odysseus_tree/package/probe-rs/probe-rs.mk
@@ -4,7 +4,7 @@ PROBE_RS_SITE = https://github.com/probe-rs/probe-rs
 PROBE_RS_GIT_SUBMODULES = YES
 PROBE_RS_DEPENDENCIES = host-rustc libusb udev
 PROBE_RS_CARGO_BUILD_OPTS = -p probe-rs-tools --features remote
-PROBE_RS_CARGO_INSTALL_OPTS = probe-rs-tools
+PROBE_RS_CARGO_INSTALL_OPTS = --path ./probe-rs-tools
 
 
 define PROBE_RS_INSTALL_INIT_SYSV
